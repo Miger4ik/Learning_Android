eb661d8fcb85a8154c617bd6464df6813c4e7220
Migera <miger4ik1590@gmail.com>
2020-03-04 21:07:04 +0200

    fix code and add new function

--- MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     // create all fields for text view
     private EditText userName;
     private TextView orderPrice;
@@ -17,25 +24,30 @@ public class MainActivity extends AppCompatActivity {
     private Button addItem;
     private Button minusItem;
     private Button addToCart;
+    private Spinner spinner;
     // fields for inner logic
+    private List<String> spinnerArrayList = new ArrayList<>();
+    private HashMap<String, Integer> goodsMap = new HashMap<>();
+    //
+    private String goodsName;
     private static int quantityItems;
     private static int price;
+    //
+    ArrayAdapter spinnerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // find all text element by ID
-        userName = findViewById(R.id.user_name_edit_text);
-        orderPrice = findViewById(R.id.order_price_text);
-        quantity = findViewById(R.id.quantity_text);
-        // find all button by ID
-        addItem = findViewById(R.id.button_add_item);
-        minusItem = findViewById(R.id.button_minus_item);
-        addToCart = findViewById(R.id.button_add_to_cart);
+
+        findView();
+        createGoodMap();
+        createSpinner();
+
         // set value price and quantity in variables
         quantityItems = Integer.parseInt(quantity.getText().toString());
         price = Integer.parseInt(orderPrice.getText().toString());
-
+        //
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,16 +62,70 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    void createSpinner () {
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinner.setOnItemSelectedListener(this);
+        spinner.setAdapter(spinnerAdapter);
+    }
+
+    void createGoodMap () {
+        goodsMap.put("guitar", 500);
+        goodsMap.put("drums", 1500);
+        goodsMap.put("keyboard", 1000);
+    }
+    //
+    void findView () {
+        // find all text element by ID
+        userName = findViewById(R.id.user_name_edit_text);
+        orderPrice = findViewById(R.id.order_price_text);
+        quantity = findViewById(R.id.quantity_text);
+        // find all button by ID
+        addItem = findViewById(R.id.button_add_item);
+        minusItem = findViewById(R.id.button_minus_item);
+        addToCart = findViewById(R.id.button_add_to_cart);
+        spinner = findViewById(R.id.spinner_with_items);
+    }
+
+    void changePrice () {
+        orderPrice.setText(String.valueOf(price * quantityItems));
+    }
+
+    void changeQuantity (int quantityOfInstruments) {
+        quantity.setText(String.valueOf(quantityOfInstruments));
+    }
+
     // method for increment quantity item
     private void incrementItem(){
         quantityItems ++;
-        quantity.setText(String.valueOf(quantityItems));
+        changeQuantity(quantityItems);
+        changePrice();
     }
+
     // method for decrement quantity item
     private void decrementItem(){
         if(quantityItems > 0) {
             quantityItems --;
-            quantity.setText(String.valueOf(quantityItems));
+            changeQuantity(quantityItems);
+            changePrice();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = goodsMap.get(goodsName);
+        changePrice();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
